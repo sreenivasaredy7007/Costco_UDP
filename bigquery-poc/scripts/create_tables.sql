b262ffc891cb79c1cc3c02a6384bbe9df32de1ca
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-10-25 14:45:16 +0530

    Update create_tables.sql

--- bigquery-poc/scripts/create_tables.sql
+++ bigquery-poc/scripts/create_tables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS `your_project_id.dataset_name.table_name` (
-  id INT64,
-  name STRING,
-  age INT64
-);
+-- CREATE TABLE IF NOT EXISTS `your_project_id.dataset_name.table_name` (
+--   id INT64,
+--   name STRING,
+--   age INT64
+-- );
