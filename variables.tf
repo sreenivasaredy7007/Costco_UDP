2ae5b7cd9702ec66d42a6ad0258f24ceaf04dc9e
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-11-13 18:33:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,17 +3,30 @@ variable "project_id" {
   type        = string
 }
 
-variable "topic_name" {
-  description = "Pub/Sub topic name"
-  type        = string
-  default     = "my-topic"
+#variable "topic_name" {
+#  description = "Pub/Sub topic name"
+#  type        = string
+#  default     = "my-topic"
+#}
+#
+#variable "subscription_name" {
+#  description = "Pub/Sub subscription name"
+#  type        = string
+#  default     = "my-subscription"
+#}
+
+variable "create_topic" {
+  description = "Flag to control creation of the Pub/Sub topic"
+  type        = bool
+  default     = true  # Set to false if the topic already exists
 }
 
-variable "subscription_name" {
-  description = "Pub/Sub subscription name"
-  type        = string
-  default     = "my-subscription"
+variable "create_subscription" {
+  description = "Flag to control creation of the Pub/Sub subscription"
+  type        = bool
+  default     = true  # Set to false if the subscription already exists
 }
+
 
 variable "service_account_email" {
   description = "Service account email for authentication"
