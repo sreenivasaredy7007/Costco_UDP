6ebf9fced10a84ed7e754dfc4a6c024495abf060
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-11-26 15:11:46 +0530

    Update main.tf
    Updted the dataproc cluster details

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -1,16 +1,35 @@
-data "template_file" "dataproc_template" {
-  template = file("${path.module}/dataproc.tf.tpl")
+#data "template_file" "dataproc_template" {
+#  template = file("${path.module}/dataproc.tf.tpl")
+#
+#  vars = {
+#    cluster_name = var.cluster_name
+#    region       = var.region
+#  }
+#}
+#
+#resource "google_dataproc_cluster" "cluster" {
+#  count    = 1
+#  provider = google
+#
+#  # Rendered template content applied here
+#  content = data.template_file.dataproc_template.rendered
+#}
 
-  vars = {
-    cluster_name = var.cluster_name
-    region       = var.region
-  }
-}
+#------------------------------------------------------------------------#
 
 resource "google_dataproc_cluster" "cluster" {
-  count    = 1
-  provider = google
+  name   = var.cluster_name
+  region = var.region
 
-  # Rendered template content applied here
-  content = data.template_file.dataproc_template.rendered
+  cluster_config {
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-standard-4"
+    }
+
+    worker_config {
+      num_instances = 2
+      machine_type  = "n1-standard-4"
+    }
+  }
 }
