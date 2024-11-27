6d1f3d303fc559c54fa569c04421d4a97ad97c3e
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-11-27 09:47:52 +0530

    Create outputs.tf
    To know the status of results.

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "topic_name" {
+  description = "Name of the created Pub/Sub topic"
+  value       = var.topic_name
+}
+
+output "subscription_name" {
+  description = "Name of the created Pub/Sub subscription"
+  value       = var.subscription_name
+}
