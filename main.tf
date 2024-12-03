d3ca14fb87e54ac21fe6f078230a99b5a298803d
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-12-03 23:53:36 +0530

    Update main.tf
    blocker the bucket creation

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "example_bucket" {
-  name          = "my-things-create"
-  location      = "US"
-  storage_class = "STANDARD"
-}
-
-# Simulate a folder by creating a placeholder object
-resource "google_storage_bucket_object" "folder" {
-  name    = "my-bucket-folder/"  # The trailing slash is important to simulate a folder.
-  bucket  = google_storage_bucket.example_bucket.name  # Correct reference to the bucket.
-  content = ""  # Empty content to act as a placeholder.
-}
+#resource "google_storage_bucket" "example_bucket" {
+#  name          = "my-things-create"
+#  location      = "US"
+#  storage_class = "STANDARD"
+#}
+#
+## Simulate a folder by creating a placeholder object
+#resource "google_storage_bucket_object" "folder" {
+#  name    = "my-bucket-folder/"  # The trailing slash is important to simulate a folder.
+#  bucket  = google_storage_bucket.example_bucket.name  # Correct reference to the bucket.
+#  content = ""  # Empty content to act as a placeholder.
+#}
