dd2ab685ba2c304c95427e17e5239efeafb17170
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-12-04 13:57:39 +0530

    Update main.tf
    Commented simulate folder

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ resource "google_storage_bucket" "example_bucket" {
   storage_class = "STANDARD"
 }
 
-# Simulate a folder by creating a placeholder object
-resource "google_storage_bucket_object" "folder" {
-  name    = "my-bucket-folder/"  # The trailing slash is important to simulate a folder.
-  bucket  = google_storage_bucket.example_bucket.name  # Correct reference to the bucket.
-  content = ""  # Empty content to act as a placeholder.
-}
+## Simulate a folder by creating a placeholder object
+#resource "google_storage_bucket_object" "folder" {
+#  name    = "my-bucket-folder/"  # The trailing slash is important to simulate a folder.
+#  bucket  = google_storage_bucket.example_bucket.name  # Correct reference to the bucket.
+#  content = ""  # Empty content to act as a placeholder.
+#}
